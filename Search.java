50220ed83fedfdf28a3d03d4439afc29c4e6930a
JLME19 <115184005+JLME19@users.noreply.github.com>
2023-10-20 03:36:14 +0000

    binarysearch

---
+++ Search.java
@@ -0,0 +1,68 @@
+public class Search {
+
+    public static void main(String[] args) {
+       
+    int[] arr1= {10,20,30,40,50};
+
+    
+ System.out.println(binarySearch(arr1, 10));
+System.out.println(binarySearch(arr1, 50));
+System.out.println(binarySearch(arr1, 30));
+System.out.println(binarySearch(arr1, 18));
+    }
+
+
+    private static int binarySearch(int[]numbers,int target) {
+
+    int min= 0;
+    int max= numbers.length-1;
+
+    while (min<=max){
+
+        int middle= (min+max)/2;
+        int middleNumber= numbers[middle];
+
+        if (target<middleNumber){
+
+            max= middle-1;
+
+        }
+        else if (target>middleNumber){
+            min= middle+1;
+        }
+       else {
+        return middle;
+       }
+
+    } return-1;
+
+    }
+
+
+private static int linearSearch(int[]numbers,int target){
+
+for();
+
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
